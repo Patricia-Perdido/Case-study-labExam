e2a1728e76c63cb3558841f44218e919f025e005
Jenyvi <jenyvi.pena13@gmail.com>
2021-03-21 06:10:52 -0700

    Final Commit

--- Method.java
+++ Method.java
@@ -12,14 +12,19 @@ package method;
 public class Method{ 
     public static void main (String[] args) {
     
- 
+ //Perdido's Method
 sayNameMethod();
 genderMethod();
 int year = BirthYear();
 System.out.println ("My birthyear: "+year);
 System.out.println ("------------------");
 
-
+// Pena, Jenyvi
+sayMyName(" Ma.  Jenyvi M.  Peña");
+addressMethod();
+int age = returnTwentyone();
+System.out.println ("Age: " + age);
+//Basic Information
 }
     static int BirthYear(){
     return 2001;
@@ -29,6 +34,15 @@ System.out.println ("------------------");
   }
   public static void genderMethod(){
       System.out.println("Gender: Female");
+	}
+static int returnTwentyone (){
+return 21;
+}
+static void sayMyName(String name) {
+System.out.println ("My name is: " + name);
+} 
+static void addressMethod() {
+System.out.println ("Address: Tarlac City ") ;
   }
 
 }
